33a456f795b1bbc68a28d4c119d2fe849b72f874
zhangjiang <896884553@q.com>
2017-01-05 14:40:39 +0800

    Initial Commit

---
+++ MyCustomProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCustomProject
+//
+//  Created by pg on 2017/1/5.
+//  Copyright © 2017年 DZHFCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
